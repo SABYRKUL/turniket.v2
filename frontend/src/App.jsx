4540783b3b935978779f274a7d0e716211dda298
Your Name <you@example.com>
2025-04-23 14:01:06 +0600

    'add_header'

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/header/Header'
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="main-content">
+        {/* Здесь будет основной контент */}
+      </main>
+    </>
+  )
+}
+export default App
